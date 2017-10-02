2883bb9930fd737a0c2a85df06b15be58ef2711a
Yilmaz Guleryuz <yilmaz@guleryuz.net>
2017-10-02 21:08:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {
+  Admin, Resource, //Delete,
+  jsonServerRestClient,
+} from 'admin-on-rest';
+
+import PostIcon from 'material-ui/svg-icons/action/book';
+import CommentIcon from 'material-ui/svg-icons/communication/chat-bubble';
+import { PostList, PostCreate, PostEdit, PostShow } from './posts';
+import { CommentList, CommentEdit, CommentCreate, CommentShow } from './comments';
+
+import translations from './i18n';
+
+const App = () => (
+    <Admin
+      title={'Learning-by-Doing'}
+      restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}
+      locale="en" messages={translations}
+      >
+
+    <Resource name="posts"
+      list={PostList} show={PostShow}
+      //create={PostCreate} edit={PostEdit}  remove={Delete}
+      icon={PostIcon} />
+    <Resource name="comments"
+      list={CommentList} show={CommentShow}
+      //create={CommentCreate}  edit={CommentEdit} remove={Delete}
+      icon={CommentIcon} />
+    <Resource name="tags" />
+
+    </Admin>
+);
+
+export default App;
---
+++ src/VisExample.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {
+  XYPlot,
+  LineSeries, VerticalBarSeries, MarkSeries,
+  VerticalGridLines, HorizontalGridLines,
+  XAxis, YAxis
+} from 'react-vis';
+
+class App extends Component {
+  render() {
+    const data = [
+      {x: 0, y: 8},
+      {x: 1, y: 5},
+      {x: 2, y: 4},
+      {x: 3, y: 9},
+      {x: 4, y: 1},
+      {x: 5, y: 7},
+      {x: 6, y: 6},
+      {x: 7, y: 3},
+      {x: 8, y: 2},
+      {x: 9, y: 0}
+    ];
+    return (
+      <div className="App">
+        <XYPlot height={300} width= {300}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineSeries data={data} />
+        </XYPlot>
+        <XYPlot height={200} width={200}>
+          <VerticalBarSeries data={data} />
+        </XYPlot>
+        <XYPlot height={200} width={200}>
+          <MarkSeries data={data} />
+        </XYPlot>
+      </div>
+    );
+  }
+}
+
+export default App;
