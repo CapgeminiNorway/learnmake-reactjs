268b79a9019277ffa39384038c13cfd21f45a194
Yilmaz Guleryuz <yilmaz@guleryuz.net>
2017-10-03 10:43:44 +0200

    deploy on Heroku

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// This one is already present in admin-on-rest Layout, but it seems it does nothing if called after the initial ReactDOM.render()
+// @link https://github.com/callemall/material-ui/issues/4670#issuecomment-235031917
+import injectTapEventPlugin from 'react-tap-event-plugin';
+try {
+    injectTapEventPlugin();
+} catch (e) {
+    // do nothing
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
